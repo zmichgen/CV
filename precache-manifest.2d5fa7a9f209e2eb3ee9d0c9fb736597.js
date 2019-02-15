a3f1e0de1d5720400b455fa5ec885e7a7751cb3c
zmichgen <zmichgen@gmail.com>
2019-02-15 12:48:46 +0300

    Updates

---
+++ precache-manifest.2d5fa7a9f209e2eb3ee9d0c9fb736597.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "6f688047c2198166b4bff2839a27cf7e",
+    "url": "/CV/static/media/azoft-sans.6f688047.ttf"
+  },
+  {
+    "revision": "59efc794449348d122c33c9f89210854",
+    "url": "/CV/static/media/Lato-Medium.59efc794.ttf"
+  },
+  {
+    "revision": "b56a6929e4b727684902",
+    "url": "/CV/static/js/runtime~main.b56a6929.js"
+  },
+  {
+    "revision": "4e2f054ddc18bd38096f",
+    "url": "/CV/static/js/main.4e2f054d.chunk.js"
+  },
+  {
+    "revision": "6abcb3a92f5e97831843",
+    "url": "/CV/static/js/2.6abcb3a9.chunk.js"
+  },
+  {
+    "revision": "4e2f054ddc18bd38096f",
+    "url": "/CV/static/css/main.686f7c25.chunk.css"
+  },
+  {
+    "revision": "6abcb3a92f5e97831843",
+    "url": "/CV/static/css/2.c58c7c2f.chunk.css"
+  },
+  {
+    "revision": "59aa97889909b7348f0202e8048ebbd6",
+    "url": "/CV/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/zmichgen/CV/precache-manifest.f91ef8e6f74f7c775f3cdedcf8a6840c.js"
+  "/CV/precache-manifest.2d5fa7a9f209e2eb3ee9d0c9fb736597.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/zmichgen/CV/index.html", {
+workbox.routing.registerNavigationRoute("/CV/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
