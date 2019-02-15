157a8d41e4416c106414581c7f2ef3e8c926dea8
zmichgen <zmichgen@gmail.com>
2019-02-15 12:46:42 +0300

    Updates

---
+++ precache-manifest.f91ef8e6f74f7c775f3cdedcf8a6840c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "6f688047c2198166b4bff2839a27cf7e",
+    "url": "/zmichgen/CV/static/media/azoft-sans.6f688047.ttf"
+  },
+  {
+    "revision": "59efc794449348d122c33c9f89210854",
+    "url": "/zmichgen/CV/static/media/Lato-Medium.59efc794.ttf"
+  },
+  {
+    "revision": "2f5fda70f7310837005f",
+    "url": "/zmichgen/CV/static/js/runtime~main.2f5fda70.js"
+  },
+  {
+    "revision": "85c3442c0c2cdbea6c66",
+    "url": "/zmichgen/CV/static/js/main.85c3442c.chunk.js"
+  },
+  {
+    "revision": "6abcb3a92f5e97831843",
+    "url": "/zmichgen/CV/static/js/2.6abcb3a9.chunk.js"
+  },
+  {
+    "revision": "85c3442c0c2cdbea6c66",
+    "url": "/zmichgen/CV/static/css/main.f7737e50.chunk.css"
+  },
+  {
+    "revision": "6abcb3a92f5e97831843",
+    "url": "/zmichgen/CV/static/css/2.c58c7c2f.chunk.css"
+  },
+  {
+    "revision": "bff48525fb62b01f49a6a7b9ab121f7f",
+    "url": "/zmichgen/CV/index.html"
+  }
+];
